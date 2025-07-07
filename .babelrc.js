c69511fa4ffc4c3da5f753312c1fd56e35adbf80
thehugegatsby <thehugegatsby@gmail.com>
2025-07-07 15:56:34 +0200

    fix: Fix React not defined error in production build
    - Add explicit React import to _app.tsx
    - Configure Babel preset-react with automatic runtime for React 17+
    - Fixes build error that occurred during GitHub Actions CI/CD

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    '@babel/preset-react',
+    ['@babel/preset-react', { runtime: 'automatic' }],
     [
       '@babel/preset-env',
       {
@@ -18,7 +18,7 @@ module.exports = {
     test: {
       presets: [
         '@babel/preset-typescript',
-        '@babel/preset-react', 
+        ['@babel/preset-react', { runtime: 'automatic' }], 
         [
           '@babel/preset-env',
           {
